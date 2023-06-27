6b8eaf7eadaa52781b26ad825d1545c96bbf2850
MichaelCookie <michael.cook@mykenzie.snhu.edu>
2023-06-27 09:06:36 -0400

    completed book annotations

--- DynamoDB/Annotations/src/main/java/com/kenzie/dynamodb/annotationsloadsave/annotations/Book.java
+++ DynamoDB/Annotations/src/main/java/com/kenzie/dynamodb/annotationsloadsave/annotations/Book.java
@@ -2,12 +2,46 @@ package com.kenzie.dynamodb.annotationsloadsave.annotations;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBRangeKey;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+
+@DynamoDBTable(tableName = "DynamoDbAnnotationsLoadSave-Books")
 
 public class Book {
     private String asin;
     private String author;
     private String title;
     private Integer yearPublished;
+    @DynamoDBHashKey(attributeName = "asin")
+    public String getAsin() {
+        return asin;
+    }
 
+    public void setAsin(String asin) {
+        this.asin = asin;
+    }
+    @DynamoDBAttribute(attributeName = "author")
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    @DynamoDBAttribute(attributeName = "title")
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    @DynamoDBAttribute(attributeName = "year_published")
+    public Integer getYearPublished() {
+        return yearPublished;
+    }
+
+    public void setYearPublished(Integer yearPublished) {
+        this.yearPublished = yearPublished;
+    }
 }
